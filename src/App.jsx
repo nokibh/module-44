2dd30b8a2c21b4a69bb4ea428f080e09bc4acee7
nokibh <nokibhowlader2003@gmail.com>
2024-03-21 16:15:33 +0600

    nam toggolle add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css';
-
+import NavBer from './Components/NavBer/NavBer';
+import DaisyNav from './Components/DaisyNav/DaisyNav';
 function App() {
   return (
     <>
+      <NavBer></NavBer>
+      <DaisyNav></DaisyNav>
       <h1 className="text-7xl bg-slate-100">Vite + React</h1>
     </>
   );
